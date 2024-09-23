eda9a89e61be1bccf4a561fdba3a72a6822c3988
Mohit Acharya <mohitacharya100@gmail.com>
2024-09-23 22:00:56 +0545

    feat: show latest dates at first

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import { Link } from 'gatsby';
 import forexData from '../data/forex_data.json';
 
 const HomePage = () => {
+  // Reverse the data to show latest dates first
+  const sortedForexData = [...forexData].reverse();
+
   return (
     <div className="container mx-auto py-6">
       <h1 className="text-2xl font-bold mb-4">Forex Rates</h1>
       <ul className="list-disc pl-5">
-        {forexData.map((data) => (
+        {sortedForexData.map((data) => (
           <li key={data.date}>
             <Link to={`/forex/${data.date}`} className="text-blue-500 hover:underline">
               Forex Rates for {data.date}
